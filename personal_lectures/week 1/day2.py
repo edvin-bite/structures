1b45578876c6a25accc0f3c5f97de41cd33bbfa6
edvin-bite <snackpackbpg@gmail.com>
2026-08-26 16:04:10 -0400

    preped for forking git repo

---
+++ personal_lectures/week 1/day2.py
@@ -0,0 +1,22 @@
+"""
+Description:
+
+
+Notes:
+language is associating something (tokens) with meaning, 
+    
+"""
+
+from typing import Any
+
+
+def eval(statement: Any):
+    # checking the statement uses valid tokens
+    for t in statement: # t for token
+        assert t in "-0123456789"
+    
+def test_eval():
+    pass
+if __name__ == "__main__":
+    test_eval()
+    print("Done.")
