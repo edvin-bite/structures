2ed44ce7f00080f30cca39e5a0e87b0795668c07
edvin-bite <snackpackbpg@gmail.com>
2026-09-02 17:00:17 -0400

    copy of class code, disliked by pylance, technically works though...

--- personal_lectures/week 2/tokenizer.py
+++ personal_lectures/week 2/tokenizer.py
@@ -17,18 +17,20 @@ import re
 patterns = [
     (r"\d+", "number"), # '\d' for ascii digit & '+' for regex to match any number of preceding regex to the tag
     (r"\s+", "whitespace"), # '\s' is ascii whitespace/space
-    (r"\+", "+"),
-    (r"\-", "-"),
-    (r"\*", "*"),
-    (r"\/", "/"),
+    (r"\+", "addition"),
+    (r"\-", "subtraction"),
+    (r"\*", "multiplication"),
+    (r"\/", "division"),
    #(r"\%", ""),
-    (r"\(", "("),
-    (r"\)", ")"),
+    (r"\(", "open_par"),
+    (r"\)", "close_par"),
 ]
 
 patterns = [(re.compile(p), tag) for p, tag in patterns] # I hate python
 
-def tokenize(chars):
+# print(patterns) 
+
+def tokenize(chars: str):
     """
     Tokenizes a string for use by the parser. matches patterns with token tags needed by the parser for syntatic step
 
@@ -36,4 +38,51 @@ def tokenize(chars):
         chars (_type_): _description_
     """
     tokens = []
-    
+    position = 0
+    line = 1
+    column = 1
+    cur_tag = None
+    # Pylance really does not like his stylistic choices. so much use of side effects.
+    while position < len(chars):
+        for pattern , tag in patterns:
+            match = pattern.match(chars, position)
+            if match:
+                cur_tag = tag
+                break
+        assert match is not None
+        value = match.group(0)
+        
+        if cur_tag == "error":
+            raise Exception(f"unexpected character: {value!r}")
+        
+        if cur_tag != "whitespace":
+            token = {"tag": cur_tag, "line":line,"column":column }
+            if cur_tag == "number":
+                token["value"] = int(value)
+            tokens.append(token)
+        
+        for ch in value:
+            if ch == "\n":
+                line += 1
+                column = 1
+            else:
+                column += 1
+        
+        position = match.end()
+        # print(value)
+        # print(cur_tag)
+        # print()
+    tokens.append({"tag": None, "line": line, "column": column})
+    return tokens
+        
+
+def test():
+    print("test general:\n")
+    try:
+        tokenize("145 + 3 - 48")
+    except Exception as e:
+        print("error...")
+
+if __name__=="__main__":
+    test()
+    print("done")
